6366347b86fcbfcdf21421d738103677b69ce1df
luke01455 <luke0145555@hotmail.co.uk>
2020-01-29 17:03:19 +0000

    edited proxy homepage and uri

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks'
 import { setContext } from 'apollo-link-context'
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:5000'
+    uri: 'https://quizapplicationserver.herokuapp.com/'
 })
 
 // will set the jwt token as the authorization header
